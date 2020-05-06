d948115bbb58c882a8cf3dbc4a163683576890a1
MITHILESH <monikasingh@Monikas-MacBook-Pro.local>
2020-05-06 20:36:44 +0530

    New module added - Module 6 Trying to make Emi calculator

---
+++ Module6-Emi Calculator/Module6-Emi Calculator/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Module6-Emi Calculator
+//
+//  Created by Monika Singh on 06/05/20.
+//  Copyright © 2020 com.phomotecg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
